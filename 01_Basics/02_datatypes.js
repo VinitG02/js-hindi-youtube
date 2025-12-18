38fc1907254f74216b24f91bdd5035d6d79b3c84
Vineet Gajadiwar <154998305+VinitG02@users.noreply.github.com>
2025-12-18 04:53:31 +0000

    DATATYPESS

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"
+// alert("i am vineet")
+
+let age = 23;
+let name = "Vineet";
+let haveDrivingLic = true
+let sem3Pass = undefined
+
+console.log(typeof age)
+console.log(typeof sem3Pass)
+console.log(typeof haveDrivingLic);
+
+
+/*
+number   2 *56
+bigint -> large number
+boolean -> true/false // 0-1
+null - > standalonevalue, representation off empty value, which is done by INTENTIONALLY
+undefined -> if any value not assign so it is undefined,
+symbol -> find the uniqueness
+
+
+
+object ->
+cons
+*/
