5717312fa86dee8fe4c2f852ba9361ce28a695c7
Vineet Gajadiwar <154998305+VinitG02@users.noreply.github.com>
2025-12-18 10:31:08 +0000

    string methods

--- 01_Basics/string.js
+++ 01_Basics/string.js
@@ -1,2 +1,32 @@
 // Here we see the operations of a string 
 
+
+const name = "vineet"
+const repo = 5;
+console.log(name + repo + "Value")
+console.log(`My Name is ${name} I github my repos are ${repo}`);
+
+const gamespace = new String("VineetGajadiwar");
+console.log(gamespace)
+
+//String. Methods
+console.log(gamespace.toUpperCase())
+
+console.log(gamespace.charAt(0))
+console.log(gamespace.indexOf('r'))
+console.log(gamespace.trim())
+console.log(gamespace.length)
+// console.log(gamespace.__proto__) 
+
+// const substring = console.log(gamespace.substring(3,14))  
+
+// const anotherString = gamespace.slice(0,9)
+const anotherString = gamespace.slice(-15, -9)
+console.log(anotherString)
+
+//replace
+const url = "https://www.facebooks.com vinit";
+console.log(url.replace('s', ''))
+
+// is any value is present or not
+console.log(url.includes('viniddt'))
