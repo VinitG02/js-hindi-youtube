69e4360f1ba65cc70ce249eca5111001fb7daf73
Vineet Gajadiwar <154998305+VinitG02@users.noreply.github.com>
2025-12-18 07:14:47 +0000

    changs

---
+++ 01_Basics/04_RevisionDT.js
@@ -0,0 +1,23 @@
+/*
+Types of Data 
+1. Primitive -> String, Number, Boolean, null, undefined, Bigint ,Symbol
+2. Non-Primitive -> class, objects, Arrays, functions/methods
+*/
+
+
+// object
+
+heros = {
+    dabang : "SalmanKhan",
+    ravan : "SharukKhan",
+    dangal    : "am`irKhan"
+
+}
+
+
+movie = ["dabang","ravan","dangal"]
+
+watch = function(){
+
+
+}
--- 01_Basics/conversionNoperations.js
+++ 01_Basics/conversionNoperations.js
@@ -26,7 +26,7 @@ true - 1, false - 0
 
 
 
-
+/*
 let isLogedIn = 1;
 let booleanLogedIn = Boolean(isLogedIn);
 // console.log(booleanLogedIn)
@@ -36,11 +36,53 @@ let number = 68;
 let isnumber = String(number)  // 68 to "68"
 console.log(typeof isnumber) // String
 console.log(isnumber) //68
-
-
-
-
-
 let typetoNumber = Number(isnumber)
 console.log(typeof typetoNumber)
 console.log(typetoNumber + 1)
+*/
+
+// ***************************** Operations *****************************
+
+let val = 5
+let negval = -5
+console.log(negval)
+
+console.log(2 + 2)
+console.log(2 - 2)
+console.log(2 * 2)
+console.log(2 / 2)
+console.log(2 % 2)
+console.log(2 ** 3)
+
+
+let fname = "Vineet";
+let lname = " Gajadiwar"
+let fullName = (fname + lname)
+console.log(fullName)
+
+
+// let num1 = num2 = num3 = 5
+// console.log(num1)
+// console.log(num2)
+// console.log(num3)
+
+// console.log("2" == 2)
+// console.log(2 > "2");
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0)
+
+
+
+// Strict Check (not only check the value but also it check the data type off a value)
+
+// === 
+console.log("2" == 2) //f
+console.log("2" === 2) // t
