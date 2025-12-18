03565f3af056b2c7e546d47d7076e5abf55ff601
Vineet Gajadiwar <154998305+VinitG02@users.noreply.github.com>
2025-12-18 05:30:28 +0000

    TypeConversion

---
+++ 01_Basics/conversionNoperations.js
@@ -0,0 +1,46 @@
+// Conversion And Operations
+
+// let score = "7vv"
+let score = undefined
+
+//console.log(typeof score);
+// console.log(typeof (score));
+
+
+let newScore = Number(score)
+
+
+//console.log(typeof newScore)
+//console.log(newScore)
+
+
+/*
+"33" -> 33
+"33abc" - > NaN
+null - > 0
+undefined - > NaN
+true - 1, false - 0
+
+*/
+
+
+
+
+
+let isLogedIn = 1;
+let booleanLogedIn = Boolean(isLogedIn);
+// console.log(booleanLogedIn)
+// console.log(typeof booleanLogedIn)
+
+let number = 68;
+let isnumber = String(number)  // 68 to "68"
+console.log(typeof isnumber) // String
+console.log(isnumber) //68
+
+
+
+
+
+let typetoNumber = Number(isnumber)
+console.log(typeof typetoNumber)
+console.log(typetoNumber + 1)
